d6eca5883e788c3f85d315c97b5c088d6ab704a7
Summer <1163108920@qq.com>
2021-07-31 16:25:17 +0800

    练习11：双色球1

--- 11/main.py
+++ 11/main.py
@@ -3,7 +3,8 @@
 """
 import random
 
-def bet(l = []):
+
+def bet(l=[]):
     for n in range(6):
         red = random.randint(1, 33)
         l.append(red)
@@ -26,14 +27,14 @@ if __name__ == '__main__':
             result = ''
             f = open('double_color_ball.txt', 'w+')
             for i in range(len(list1)):
-                result = str(i+1) + ':' + str(list1[i])
+                result = str(i + 1) + ':' + str(list1[i])
                 f.writelines(result)
             f.close()
             print("保存成功!")
             break
         elif c == 2:
             for i in range(len(list1)):
-                print(str(i+1) + ':' + str(list1[i]))
+                print(str(i + 1) + ':' + str(list1[i]))
             break
         else:
             print("请输入正确序号！")
