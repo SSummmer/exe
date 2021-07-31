73c50cec36d97ab38c0b02d61323c46abea5369e
Summer <1163108920@qq.com>
2021-07-31 15:57:02 +0800

    练习2：求三角形面积（调用）

--- 2/main.py
+++ 2/main.py
@@ -4,10 +4,9 @@
 
 
 def cal(a, b):
-    c = a * b / 2
-    print("直角三角形的面积为", c)
+    return a * b / 2
 
 
 a = int(input("请输入直角三角形的第一条直角边边长："))
 b = int(input("请输入直角三角形的第二条直角边边长："))
-cal(a, b)
+print("直角三角形的面积为", cal(a, b))
