cac009290d5912dae4b8c0f4ef85045aaf46a172
Summer <1163108920@qq.com>
2021-08-08 16:00:38 +0800

    练习17：bilibili用户等级

--- 17/main.py
+++ 17/main.py
@@ -1,19 +1,16 @@
 """
     练习17
 """
-import numpy as np
 import pandas as pd
 
 data = pd.read_csv('bilibili_user.csv', encoding='gbk')
-data = data.replace(to_replace='', value=np.nan)
 data = data.dropna()
 
 level = data['用户的等级']
 bins = [-1, 1, 3, 5, 6]
 level_counts = pd.cut(level, bins)
 dummies = pd.get_dummies(level_counts)
-name_index = ['新用户', '普通用户', '老用户', '骨灰用户']
-dummies.columns = name_index
+dummies.columns = ['新用户', '普通用户', '老用户', '骨灰用户']
 dummies.index = dummies.index + 1
 
 print(dummies)
