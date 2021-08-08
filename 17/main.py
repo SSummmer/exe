1f5c52a71412ec13d6d05b3804e243722228717f
Summer <1163108920@qq.com>
2021-08-08 16:21:10 +0800

    练习17：bilibili用户等级

--- 17/main.py
+++ 17/main.py
@@ -4,6 +4,7 @@
 import pandas as pd
 
 data = pd.read_csv('bilibili_user.csv', encoding='gbk')
+data.index = data.index + 1
 data = data.dropna()
 
 level = data['用户的等级']
@@ -11,7 +12,6 @@ bins = [-1, 1, 3, 5, 6]
 level_counts = pd.cut(level, bins)
 dummies = pd.get_dummies(level_counts)
 dummies.columns = ['新用户', '普通用户', '老用户', '骨灰用户']
-dummies.index = dummies.index + 1
 
 print(dummies)
 print('新用户:', dummies['新用户'].value_counts()[1])
